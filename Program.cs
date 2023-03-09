739506f03af58f6b7f52745bac0e0df5c75f212a
Efrain Montealegre <efrain.montealegre@gmail.com>
2023-03-08 21:45:59 -0500

    Se agregan las tassas de interes anual efectiva

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double tasaNominal, frecuencia, tasaEfectiva, tasaEfetivaPorcentaje;
+        int n;
+
+        Console.Write("Ingrese la tasa nominal en porcentaje: ");
+        tasaNominal = double.Parse(Console.ReadLine()) / 100;
+
+        Console.Write("Ingrese la frecuencia de capitalización (por año): ");
+        frecuencia = double.Parse(Console.ReadLine());
+
+        n = (int)(frecuencia); // Obtener la cantidad de veces que se capitaliza en un año
+        tasaEfectiva = Math.Pow(1 + (tasaNominal / n), n) - 1;
+
+        tasaEfetivaPorcentaje = tasaEfectiva * 100;
+
+        //Console.WriteLine("La tasa de interés anual efectiva es: {0:P2}", tasaEfectiva);
+        Console.WriteLine("La tasa de interés anual efectiva en Porcentaje es: " + tasaEfetivaPorcentaje);
+    }
+}
